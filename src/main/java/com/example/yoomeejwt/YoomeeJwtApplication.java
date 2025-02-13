477ac84bc9538335772a4f8abb03cd6f1a10a5aa
zakie <realzzaekkii@gmail.com>
2025-02-13 14:55:32 +0900

    first commit

---
+++ src/main/java/com/example/yoomeejwt/YoomeeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoomeejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoomeeJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoomeeJwtApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/yoomeejwt/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.yoomeejwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+
+        return "this is main page";
+    }
+}
